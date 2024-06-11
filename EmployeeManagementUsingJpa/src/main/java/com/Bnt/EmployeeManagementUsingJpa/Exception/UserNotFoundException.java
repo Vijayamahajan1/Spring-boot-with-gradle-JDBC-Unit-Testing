74b3498c529a46dcb630c876aa526769f8d411c4
Vijayamahajan1 <vijaya.mahajan@bnt-soft.com>
2024-06-11 12:29:39 +0530

    add

--- EmployeeManagementUsingJpa/src/main/java/com/Bnt/EmployeeManagementUsingJpa/Exception/UserNotFoundException.java
+++ EmployeeManagementUsingJpa/src/main/java/com/Bnt/EmployeeManagementUsingJpa/Exception/UserNotFoundException.java
@@ -1,6 +1,7 @@
 package com.Bnt.EmployeeManagementUsingJpa.Exception;
 
 public class UserNotFoundException extends RuntimeException {
+    
     public UserNotFoundException(String msg, Integer id){
         super("User not Found with id:"+id);
     }
